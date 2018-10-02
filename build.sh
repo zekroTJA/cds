b14406bf6a9e816dfea59458185bcd1d2a22a55d
Ringo Hoffmann <riho@live.de>
2018-10-02 08:51:01 +0200

    updated travis script

--- build.sh
+++ build.sh
@@ -10,22 +10,22 @@ fi
 
 echo "Building linux_arm..."
 (env GOOS=linux GOARCH=amd64 \
-    go build -o cds_linux_arm \
+    go build -o builds/cds_linux_arm \
     -ldflags "-X main.appDate=$DATE -X main.appVersion=$TAG -X main.appCommit=$COMMIT")
 
 echo "Building linux_amd64.."
 (env GOOS=linux GOARCH=arm \
-    go build -o cds_linux_amd64 \
+    go build -o builds/cds_linux_amd64 \
     -ldflags "-X main.appDate=$DATE -X main.appVersion=$TAG -X main.appCommit=$COMMIT")
 
 echo "Building win_amd64..."
 (env GOOS=windows GOARCH=amd64 \
-    go build -o cds_win_amd64.exe \
+    go build -o builds/cds_win_amd64.exe \
     -ldflags "-X main.appDate=$DATE -X main.appVersion=$TAG -X main.appCommit=$COMMIT")
 
 echo "Building mac_amd64..."
 (env GOOS=darwin GOARCH=amd64 \
-    go build -o cds_mac_amd64 \
+    go build -o builds/cds_mac_amd64 \
     -ldflags "-X main.appDate=$DATE -X main.appVersion=$TAG -X main.appCommit=$COMMIT")
 
 wait
